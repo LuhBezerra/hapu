97fee7cf8879cb429bb63d7b78bea10eb08287c7
LuhBezerra <lucilene.bezerra@audora.com.br>
2021-07-10 12:56:14 -0300

    feat(ProductDescription): add responsive styling

--- src/sections/ProductDescription/index.tsx
+++ src/sections/ProductDescription/index.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 
+import { PRODUCT_DESCRIPTION } from './constants';
 import './styles.css';
 
 export function ProductDescriptionSection() {
   return (
     <section id="product-description">
       <div className="container">
-        <h1>Product Description</h1>
+        <div className="product-description-content">
+          <img
+            className="product-description-image-content"
+            src={PRODUCT_DESCRIPTION.image}
+            alt="nanny information selected on the map"
+          />
+
+          <div className="product-description-text-content">
+            <h2>{PRODUCT_DESCRIPTION.title}</h2>
+            <p>{PRODUCT_DESCRIPTION.description}</p>
+
+            <a href="/">{PRODUCT_DESCRIPTION.link}</a>
+          </div>
+        </div>
       </div>
     </section>
   );
