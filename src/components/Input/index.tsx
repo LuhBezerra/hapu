250cfbde30ae535f3f3280479781b54df39c41ac
LuhBezerra <lucilene.bezerra@audora.com.br>
2021-07-11 17:06:54 -0300

    feat(Input): create component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,24 @@
+import React, { InputHTMLAttributes } from 'react';
+
+import './styles.css';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  placeholder: string;
+}
+
+const Input: React.FC<InputProps> = ({ name, placeholder, ...rest }) => {
+  return (
+    <div className="input-block">
+      <input
+        placeholder={placeholder}
+        type="text"
+        id={name}
+        min="1"
+        {...rest}
+      />
+    </div>
+  );
+};
+
+export default Input;
